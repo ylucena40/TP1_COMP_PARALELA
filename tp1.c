e1cd62410a3dd87bb8ab531f87b20d6a14d1892b
ylucena40 <ylucen40a@gmail.com>
2021-10-05 23:09:48 -0300

    commit

--- tp1.c
+++ tp1.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void verifica_primo(int vetor[],int sizeVet){
+void verifica_primo(int vetor[],int sizeVet, FILE* arq){
+    arq = fopen("saida.txt","r");
     for(int i = 0; i < sizeVet; i++){
         if( vetor[i] != 1){
             int cont = 0;
-            for(int j = 2; j < vetor[i] / 2; j++){
+            for(int j = 2; j < (vetor[i]/2)+1; j++){
                 if(vetor[i] % j == 0){
                     cont++;
-                    break;
                 }
             }
             if(cont == 0){
-                printf("O numero: %d eh primo.\n",vetor[i]);
+                fprintf(arq,"%d\n",2);
             }
             else{
-                printf("O numero: %d nao eh primo.\n",vetor[i]);
+                fprintf(arq,"%d\n",cont+2);
             }
         }
     }
+    
+    fclose(arq);
 }
 
 
 int main(int argc, char* argv[]){
     FILE* entryfile;
+    FILE* exitfile;
     if ((entryfile = fopen(argv[1],"r")) == NULL)
     {
       printf("Error on file opening! \n");
